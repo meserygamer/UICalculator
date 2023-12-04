7e230a7a682337de9bdfb6d6c102b303c34ecd05
MeseryGG <Meserygamer@gmail.com>
2023-12-05 02:19:17 +0300

    Функционал калькулятора готов

---
+++ UICalculator/Models/MathematicalAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UICalculator.Models
+{
+    public class MathematicalFunction
+    {
+        public MathematicalFunction(Func<double, double, double> func
+            , string functionMark
+            , string functionName)
+        {
+            Func = func;
+            FunctionMark = functionMark;
+            FunctionName = functionName;
+        }
+
+        public string FunctionMark { get; set; }
+        public string FunctionName { get; set; }
+
+        public Func<double, double, double> Func { get; set; }
+
+
+        public override string ToString()
+        {
+            return FunctionName;
+        }
+    }
+}
--- UICalculator/ViewModels/CalculatorUCViewModel.cs
+++ UICalculator/ViewModels/CalculatorUCViewModel.cs
@@ -1,11 +1,65 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reactive;
 using ReactiveUI;
+using UICalculator.Models;
 
 namespace UICalculator.ViewModels
 {
 	public class CalculatorUCViewModel : ViewModelBase
 	{
+		public CalculatorUCViewModel()
+		{
+			Value1 = 0;
+			Value2 = 0;
+			Result = 0;
+			SelectedFunction = AvailableMathematicalActions[0];
+			CalculateMathFunctionCommand = ReactiveCommand.Create<string>(CalculateMathFunctionCommandRun);
+		}
 
+
+		public double Value1
+		{
+			get => _value1;
+			set => this.RaiseAndSetIfChanged(ref _value1, value);
+		}
+		public double Value2
+		{
+			get => _value2;
+			set => this.RaiseAndSetIfChanged(ref _value2, value);
+		}
+		public double Result
+		{
+			get => _result;
+			set => this.RaiseAndSetIfChanged(ref _result, value);
+		}
+
+		public MathematicalFunction SelectedFunction
+		{
+			get => _selectedFunction;
+			set => this.RaiseAndSetIfChanged(ref _selectedFunction, value);
+		}
+
+		public ObservableCollection<MathematicalFunction> AvailableMathematicalActions { get; init; }
+			= new ObservableCollection<MathematicalFunction>()
+			{
+				new MathematicalFunction((x1,x2) => x1 + x2, "+", "Сложение"),
+				new MathematicalFunction((x1,x2) => x1 - x2, "-", "Вычитание"),
+				new MathematicalFunction((x1,x2) => x1 * x2, "*", "Умножение"),
+				new MathematicalFunction((x1,x2) => x1 / x2, "/", "Деление"),
+			};
+
+		public ReactiveCommand<string, Unit> CalculateMathFunctionCommand { get; }
+
+
+        public void CalculateMathFunctionCommandRun(string parameter) => Result = SelectedFunction.Func.Invoke(Value1, Value2);
+
+
+        private double _value1;
+		private double _value2;
+		private double _result;
+
+		private MathematicalFunction _selectedFunction;
 	}
 }
