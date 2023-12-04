05d8a5a62a0040e01e0408ee906d2c575a2289cd
MeseryGG <Meserygamer@gmail.com>
2023-12-04 22:46:47 +0300

    Добавьте файлы проекта.

---
+++ UICalculator/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace UICalculator.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
