a5c4102bf71226f4df3153503846e0647353616b
MeseryGG <Meserygamer@gmail.com>
2023-12-04 23:00:01 +0300

    Сделаны загатовки под основные страницы

---
+++ UICalculator/ViewModels/AuthorizationUCViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using ReactiveUI;
+
+namespace UICalculator.ViewModels
+{
+	public class AuthorizationUCViewModel : ViewModelBase
+	{
+
+	}
+}
--- UICalculator/ViewModels/MainWindowViewModel.cs
+++ UICalculator/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,23 @@
-﻿namespace UICalculator.ViewModels
+﻿using ReactiveUI;
+
+namespace UICalculator.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public bool IsAuthorizationPage
+        {
+            get => _isAuthorizationPage;
+            set => this.RaiseAndSetIfChanged(ref _isAuthorizationPage, value);
+        }
+
+        public bool IsCalculatorPage
+        {
+            get => _isCalculatorPage;
+            set => this.RaiseAndSetIfChanged(ref _isCalculatorPage, value);
+        }
+
+        private bool _isAuthorizationPage = false;
+
+        private bool _isCalculatorPage = true;
     }
 }
